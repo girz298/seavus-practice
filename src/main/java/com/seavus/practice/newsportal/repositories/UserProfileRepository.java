9388fb8d8a7ce683575829ac01a3db7baf94b5e4
Valentine Kasatkin <valentine.kasatkin@gmail.com>
2016-06-10 18:05:11 +0300

    Mapped Article entity. Added Article Repository.

--- src/main/java/com/seavus/practice/newsportal/repositories/UserProfileRepository.java
+++ src/main/java/com/seavus/practice/newsportal/repositories/UserProfileRepository.java
@@ -1,11 +1,11 @@
 package com.seavus.practice.newsportal.repositories;
 
-import com.seavus.practice.newsportal.models.UserData;
+import com.seavus.practice.newsportal.models.UserProfile;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by MK on 09.06.2016.
  */
-public interface UserDataRepository extends CrudRepository<UserData, String> {
+public interface UserDataRepository extends CrudRepository<UserProfile, String> {
 
 }
