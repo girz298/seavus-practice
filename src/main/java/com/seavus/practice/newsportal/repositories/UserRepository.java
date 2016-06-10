57cf14423b61f1759067e4fe7dfc902dd8005fdb
Valentine Kasatkin <valentine.kasatkin@gmail.com>
2016-06-10 16:55:06 +0300

    Rewrited data model, added User class and repository

--- src/main/java/com/seavus/practice/newsportal/repositories/UserRepository.java
+++ src/main/java/com/seavus/practice/newsportal/repositories/UserRepository.java
@@ -1,10 +1,11 @@
 package com.seavus.practice.newsportal.repositories;
 
-import com.seavus.practice.newsportal.models.Project;
+import com.seavus.practice.newsportal.models.User;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by MK on 09.06.2016.
  */
-public interface ProjectRepository extends CrudRepository<Project, Long> {
+public interface UserRepository extends CrudRepository<User, String> {
+
 }
