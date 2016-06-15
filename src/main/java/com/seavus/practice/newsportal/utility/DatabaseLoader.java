5ced0e64fa8a8ee10bd0a0128c581c303d2cfb12
girz298 <girz298@gmail.com>
2016-06-15 15:56:30 +0300

    Added some news

--- src/main/java/com/seavus/practice/newsportal/utility/DatabaseLoader.java
+++ src/main/java/com/seavus/practice/newsportal/utility/DatabaseLoader.java
@@ -49,12 +49,17 @@ public class DatabaseLoader implements CommandLineRunner {
                 " Osborne says he will have to slash public spending and increase "+
                 " taxes in an emergency Budget to tackle a £30bn \"black hole\" if the UK votes to leave the "+
                 " European Union.", userProfile, LocalDateTime.now());
+        Article article1 = new Article("Alligator drags boy into water near Orlando Disney resort", "The boy was on the shoreline of the Seven Seas Lagoon by the Disney Grand Floridian Resort and Spa in Orlando when he was dragged away late on Tuesday, officials said.", userProfile, LocalDateTime.now());
+        Article article2 = new Article("US charges Chinese ex-IBM worker with espionage", "The Department of Justice did not name the employer, but it is widely reported to be software developer IBM.\n" +
+                "Mr Xu intended to sell the code for his own profit and for the benefit of the Chinese government, authorities said.", userProfile, LocalDateTime.now());
         List<Comment> articleComments = new ArrayList<>();
         articleComments.add(new Comment("amazing text1", LocalDateTime.now(), userProfile, article));
         articleComments.add(new Comment("amazing text2", LocalDateTime.now(), userProfile, article));
         article.setCommentaries(articleComments);
         List<Article> articles = new ArrayList<>();
         articles.add(article);
+        articles.add(article1);
+        articles.add(article2);
         List<Tag> articleTags = new ArrayList<>();
         Tag wow = new Tag("wow");
         wow.setBelongsToArticles(articles);
@@ -63,6 +68,6 @@ public class DatabaseLoader implements CommandLineRunner {
         doge.setBelongsToArticles(articles);
         articleTags.add(doge);
         article.setArticleTags(articleTags);
-        articleRepository.save(article);
+        articleRepository.save(articles);
     }
 }
