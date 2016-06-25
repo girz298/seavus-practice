799a3bb34fdb1f6820011d01c7247f8225349a96
sega298 <sega298@mail.ru>
2016-06-25 12:31:53 +0300

    added good bootstrap added full news view

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,7 +1,7 @@
 var newsApp = angular.module('newsApp', ['ui.router']);
 newsApp.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
 
-	// need to fix # in URL's 
+	// need to fix '#' in URL's 
 	// $locationProvider.html5Mode({
 	// 	enabled: true,
 	// 	requireBase: false
@@ -32,82 +32,34 @@ newsApp.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
 	 	    	$scope.getingStatus = false;
 		    });
 		}
+	})
+	.state('addnews',{
+		url: '/addnews',
+		templateUrl: 'templates/main.html',
+		controller: function($scope,$http,$stateParams){
+			$scope.news = [];
+			$scope.getingStatus = false;
+			$http.get("http://localhost:8080/api/tags/"+$stateParams.categoryId+"/belongsToArticles").success(function (data, status, headers, config) {
+			    $scope.news = data._embedded.articles;
+			    $scope.getingStatus = true;
+		    }).error(function () {
+	 	    	$scope.getingStatus = false;
+		    });
+		}
+	})
+	.state('full-article',{
+		url: '/articles/:articleId',
+		templateUrl: 'templates/full-article.html',
+		controller: function($scope,$http,$stateParams){
+			$scope.article = {};
+			$scope.getingStatus = false;
+			$http.get("http://localhost:8080/api/articles/"+$stateParams.articleId).success(function (data, status, headers, config) {
+			    $scope.article = data;
+			    $scope.getingStatus = true;
+		    }).error(function () {
+	 	    	$scope.getingStatus = false;
+		    });
+		}
 	});
-	// Не забудь удалить простыню 
-	// .state('sport',{
-	// 	url: '/sport',
-	// 	templateUrl: 'templates/main.html',
-	// 	controller: function($scope,$http){
-	// 		$scope.news = [];
-	// 		$http.get("http://localhost:8080/api/tags/sport/belongsToArticles").success(function (data, status, headers, config) {
-	// 		    $scope.news = data._embedded.articles;
-	// 	    });
-	// 	}
-	// })
-	// .state('culture',{
-	// 	url: '/culture',
-	// 	templateUrl: 'templates/main.html',
-	// 	controller: function($scope,$http){
-	// 		$scope.news = [];
-	// 		$scope.getingStatus = false;
-	// 		$http.get("http://localhost:8080/api/tags/memes/belongsToArticles").success(function (data, status, headers, config) {
-	// 		    $scope.news = data._embedded.articles;
-	// 		    $scope.getingStatus = true;
-	// 	    }).error(function () {
-	// 	    	$scope.getingStatus = false;
-	// 	    });
-	// 	}
-	// })
-	// .state('society',{
-	// 	url: '/society',
-	// 	templateUrl: 'templates/main.html',
-	// 	controller: function($scope,$http){
-	// 		$scope.news = [];
-	// 		$http.get("http://localhost:8080/api/tags/society/belongsToArticles").success(function (data, status, headers, config) {
-	// 		    $scope.news = data._embedded.articles;
-	// 	    });
-	// 	}
-	// })
-	// .state('policy',{
-	// 	url: '/policy',
-	// 	templateUrl: 'templates/main.html',
-	// 	controller: function($scope,$http){
-	// 		$scope.news = [];
-	// 		$http.get("http://localhost:8080/api/tags/policy/belongsToArticles").success(function (data, status, headers, config) {
-	// 		    $scope.news = data._embedded.articles;
-	// 	    });
-	// 	}
-	// })
-	// .state('economy',{
-	// 	url: '/economy',
-	// 	templateUrl: 'templates/main.html',
-	// 	controller: function($scope,$http){
-	// 		$scope.news = [];
-	// 		$http.get("http://localhost:8080/api/tags/economy/belongsToArticles").success(function (data, status, headers, config) {
-	// 		    $scope.news = data._embedded.articles;
-	// 	    });
-	// 	}
-	// })
-	// .state('other',{
-	// 	url: '/other',
-	// 	templateUrl: 'templates/main.html',
-	// 	controller: function($scope,$http){
-	// 		$scope.news = [];
-	// 		$http.get("http://localhost:8080/api/tags/wow/belongsToArticles").success(function (data, status, headers, config) {
-	// 		    $scope.news = data._embedded.articles;
-	// 	    });
-	// 	}
-	// })
-	// .state('addnews',{
-	// 	url: '/addnews',
-	// 	templateUrl: 'templates/main.html',
-	// 	controller: function($scope,$http){
-	// 		$scope.news = [];
-	// 		$http.get("http://localhost:8080/api/tags/wow/belongsToArticles").success(function (data, status, headers, config) {
-	// 		    $scope.news = data._embedded.articles;
-	// 	    });
-	// 	}
-	// });
-
 
 });
